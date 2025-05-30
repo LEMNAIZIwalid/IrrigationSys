c3bf9bcafed2a5b14da16ebf51e809d8cd07d0dc
LEMNAIZIwalid <mnizidd@gmail.com>
2025-05-30 23:54:49 +0100

    controle d'arrosage interfaces

---
+++ app/src/main/java/com/example/irrigationapp/views/ArrosageAutomatiqueActivity.java
@@ -0,0 +1,13 @@
+package com.example.irrigationapp.views;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.irrigationapp.R;
+
+public class ArrosageAutomatiqueActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_arrosage_automatique);
+    }
+}
--- app/src/main/java/com/example/irrigationapp/views/ArrosageControlActivity.java
+++ app/src/main/java/com/example/irrigationapp/views/ArrosageControlActivity.java
@@ -1,13 +1,40 @@
 package com.example.irrigationapp.views;
 
-import com.example.irrigationapp.R;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.irrigationapp.R;
+
 public class ArrosageControlActivity extends AppCompatActivity {
+
+    private Button buttonManual, buttonAuto;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_arrosage_control); // à créer plus tard
+        setContentView(R.layout.activity_arrosage_control);
+
+        buttonManual = findViewById(R.id.buttonManual);
+        buttonAuto = findViewById(R.id.buttonAuto);
+
+        buttonManual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Aller vers la page d'arrosage manuelle
+                startActivity(new Intent(ArrosageControlActivity.this, ArrosageManuelleActivity.class));
+            }
+        });
+
+        buttonAuto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Aller vers la page d'arrosage automatique
+                startActivity(new Intent(ArrosageControlActivity.this, ArrosageAutomatiqueActivity.class));
+            }
+        });
     }
 }
